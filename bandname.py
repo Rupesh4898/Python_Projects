857eef4a1f26594c5acf377c74509a598eef6a24
Rupesh <rupeshrr4895@gmail.com>
2023-02-11 22:15:27 +0530

    projects

---
+++ bandname.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+petname=input("What is the name of your Pet?")
+cityname=input("WHat is the name of your city?")
+print(f"Your band name is "+ petname + cityname)
